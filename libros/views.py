4d5bf9aac1976f66a5d548e7efbc127db25fdfc6
Carlos Soto <carlosotoia@gmail.com>
2026-03-03 11:47:50 -0300

    update

---
+++ libros/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Libro
+from .forms import LibroForm
+
+# LISTAR libros
+def listar_libros(request):
+    libros = Libro.objects.all()
+    return render(request, 'listar_libros.html', {'libros': libros})
+
+
+# CREAR libro
+def crear_libro(request):
+    if request.method == 'POST':
+        form = LibroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_libros')
+    else:
+        form = LibroForm()
+
+    return render(request, 'formulario_libro.html', {'form': form})
+
+
+# EDITAR libro
+def editar_libro(request, id):
+    libro = get_object_or_404(Libro, id=id)
+
+    if request.method == 'POST':
+        form = LibroForm(request.POST, instance=libro)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_libros')
+    else:
+        form = LibroForm(instance=libro)
+
+    return render(request, 'formulario_libro.html', {'form': form})
+
+
+# ELIMINAR libro
+def eliminar_libro(request, id):
+    libro = get_object_or_404(Libro, id=id)
+
+    if request.method == 'POST':
+        libro.delete()
+        return redirect('listar_libros')
+
+    return render(request, 'confirmar_eliminacion.html', {'libro': libro})
